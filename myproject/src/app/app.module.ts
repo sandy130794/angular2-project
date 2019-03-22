e63894dc727cef3015e042f32c4e10caf1bdb432
sandy130794 <sandy.yadav156@gmail.com>
2019-03-23 00:43:58 +0530

    changes in source file

--- myproject/src/app/app.module.ts
+++ myproject/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
